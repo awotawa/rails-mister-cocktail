5fc22124d90e5817ecebe0a900d36da6ae5f776f
Alexandre Wotawa <awotawa@gmail.com>
2020-11-12 13:11:02 +0100

    created a seed of ingredients as well as seed of cocktails, started to code the cocktails controller with it's view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+Ingredient.destroy_all
+Cocktail.destroy_all
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+drink_list = open(url).read
+drinks = JSON.parse(drink_list)
+drinks['drinks'].each do |drink|
+  Ingredient.create(name: drink['strIngredient1'])
+end
+
+Cocktail.create(name: "Gin Tonic")
+Cocktail.create(name: "Cuba Libre")
+Cocktail.create(name: "Screwdriver")
+Cocktail.create(name: "Tequila Sunrise")
+Cocktail.create(name: "Long Island")
