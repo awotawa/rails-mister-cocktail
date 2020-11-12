a87a5ba1ffe2596ad23d9fbe8df2930267ae8a89
Alexandre Wotawa <awotawa@gmail.com>
2020-11-12 17:14:25 +0100

    coded a Mr.Cocktail showing ingredients and doses

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -5,5 +5,25 @@ class CocktailsController < ApplicationController
 
   def show
     @cocktail = Cocktail.find(params[:id])
+    @doses = Dose.select { |dose| @cocktail.id == dose.cocktail_id }
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
   end
 end
