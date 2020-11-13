5752c9e0aca1a667b5aae12b41a4786b5b9a21d4
Alexandre Wotawa <awotawa@gmail.com>
2020-11-13 12:37:21 +0100

    coded the deletion of a dose and implemented the code necessary for the herokuapp

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -17,9 +17,8 @@ class DosesController < ApplicationController
 
   def destroy
     @dose = Dose.find(params[:id])
-    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose.destroy
-    redirect_to cocktail_path(@cocktail)
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   private
